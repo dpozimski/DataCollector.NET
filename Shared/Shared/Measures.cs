5d2a60aea3ddbabf8590fac0b8735be10e862c0f
SoftPower <damian.pozimski@gmail.com>
2017-05-01 12:57:00 +0200

    Shared model files
    Add shared model files to use them in both UWP and PC .NET components

---
+++ Shared/Shared/Measures.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataCollector.Device.Models
+{
+    /// <summary>
+    /// Klasa zrzeszająca pomiary pobrane z źródła zewnętrznego.
+    /// </summary>
+    public sealed class Measures
+    {
+        #region Public Properties
+        /// <summary>
+        /// Akcelerometr.
+        /// </summary>
+        public SpherePoint Accelerometer { get; set; }
+        /// <summary>
+        /// Żyroskop.
+        /// </summary>
+        public SpherePoint Gyroscope { get; set; }
+        /// <summary>
+        /// Wilgotność [RH].
+        /// </summary>
+        public float? Humidity { get; set; }
+        /// <summary>
+        /// Temperatura [*C].
+        /// </summary>
+        public float? Temperature { get; set; }
+        /// <summary>
+        /// Ciśnienie atmosferyczne [hPa].
+        /// </summary>
+        public float? AirPressure { get; set; }
+        /// <summary>
+        /// Dioda LED jest aktywna.
+        /// </summary>
+        public bool? IsLedActive { get; set; }
+        #endregion
+
+        #region Overrides
+        /// <summary>
+        /// Metoda nadpisująca metodę ToString w celu wyświetlenia zawartości obiektu.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine($"{nameof(Accelerometer)}: X={Accelerometer?.X} Y={Accelerometer?.Y}, Z={Accelerometer?.Z}");
+            builder.AppendLine($"{nameof(Gyroscope)}    : X={Gyroscope?.X} Y={Gyroscope?.Y}, Z={Gyroscope?.Z}");
+            builder.AppendLine($"{nameof(Temperature)}  :   {Temperature}");
+            builder.AppendLine($"{nameof(Humidity)}     :   {Humidity}");
+            builder.AppendLine($"{nameof(AirPressure)}  :   {AirPressure}");
+            builder.AppendLine($"{nameof(IsLedActive)}  :   {(IsLedActive.HasValue && IsLedActive.Value ? "Tak" : "Nie")}");
+            return builder.ToString();
+        }
+        #endregion
+    }
+}
---
+++ Shared/Shared/SpherePoint.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataCollector.Device.Models
+{
+    /// <summary>
+    /// Klasa reprezentująca Punkt w przestrzeni.
+    /// </summary>
+    public sealed class SpherePoint
+    {
+        #region Public Properties
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+        #endregion
+
+        #region ctor
+        /// <summary>
+        /// Konstruktor klasy Point3D.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        public SpherePoint(float x, float y, float z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+        /// <summary>
+        /// Domyślny konstruktor klasy Point3D.
+        /// </summary>
+        public SpherePoint()
+        {
+
+        }
+        #endregion
+    }
+}
