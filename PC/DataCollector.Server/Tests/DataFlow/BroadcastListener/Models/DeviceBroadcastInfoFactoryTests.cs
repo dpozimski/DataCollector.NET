23beaed3535b49ecd0a2fc1a1cf47b099f8d5131
SoftPower <damian.pozimski@gmail.com>
2017-05-03 15:27:09 +0200

    Create template for wcf service, create frame maker tests

---
+++ PC/DataCollector.Server/Tests/DataFlow/BroadcastListener/Models/DeviceBroadcastInfoFactoryTests.cs
@@ -0,0 +1,45 @@
+﻿using DataCollector.Server.DataFlow.BroadcastListener.Models;
+using DataCollector.Server.DataFlow.BroadcastListener.Models.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using static DataCollector.Server.DataFlow.BroadcastListener.Models.Exceptions.InvalidFrameException;
+
+namespace DataCollector.Server.Tests.DataFlow.BroadcastListener.Models
+{
+    /// <summary>
+    /// Klasa testująca fabrykę ramek identyfikujących urządzenie.
+    /// </summary>
+    public class DeviceBroadcastInfoFactoryTests
+    {
+        private DeviceBroadcastInfoFactory broadcastInfoFactory;
+
+        public DeviceBroadcastInfoFactoryTests()
+        {
+            broadcastInfoFactory = new DeviceBroadcastInfoFactory();
+        }
+
+        DeviceBroadcastInfo Execute(byte[] frame)
+        {
+            return broadcastInfoFactory.From(frame);
+        }
+
+        [Fact]
+        public void ParsingNullFrame()
+        {
+            byte[] frame = null;
+            Assert.Throws<ArgumentNullException>(() => Execute(frame));
+        }
+
+        [Fact]
+        public void ParsingBadSizeFrame()
+        {
+            byte[] frame = new byte[1];
+            var exception = Assert.Throws<InvalidFrameException>(() => Execute(frame));
+            Assert.Equal(ErrorType.FrameSize, exception.Type);
+        }
+    }
+}
