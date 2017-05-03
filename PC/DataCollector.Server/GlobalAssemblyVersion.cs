8a1fb9b7b04e12d67e28ebc067ddb1904987a3d6
SoftPower <damian.pozimski@gmail.com>
2017-05-03 12:08:19 +0200

    DataCollector Server solution projects

---
+++ PC/DataCollector.Server/GlobalAssemblyVersion.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.10")]
+[assembly: AssemblyTitle("RT2014 ManagerService")]
+[assembly: AssemblyCopyright("Copyright © POL-EKO-APARATURA 2016")]
+[assembly: AssemblyDescription("RT2014 ManagerService")]
+[assembly: AssemblyProduct("RT2014 ManagerService")]
