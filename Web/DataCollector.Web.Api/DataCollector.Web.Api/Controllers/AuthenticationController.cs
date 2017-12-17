2cac93feb572da2948e8cf40665bb623a147b8a1
Damian Pozimski <damian.pozimski@softpower.pl>
2017-12-17 17:56:33 +0100

    Added blank angular proj

--- Web/DataCollector.Web.Api/DataCollector.Web.Api/Controllers/AuthenticationController.cs
+++ Web/DataCollector.Web.Api/DataCollector.Web.Api/Controllers/AuthenticationController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Text;
 using DataCollector.Web.Api.Models;
 using System.Security.Claims;
